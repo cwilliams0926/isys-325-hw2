848b3645484798ceca608426c0749ce2fd54af2e
cwilliams0926 <coopwilliams03@hotmail.com>
2025-10-03 20:37:06 -0400

    Create make and model label and listbox

--- Assignment2/MainForm.Designer.cs
+++ Assignment2/MainForm.Designer.cs
@@ -28,18 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            makeAndModelLabel = new Label();
+            makeAndModelList = new ListBox();
             SuspendLayout();
+            // 
+            // makeAndModelLabel
+            // 
+            makeAndModelLabel.AutoSize = true;
+            makeAndModelLabel.Location = new Point(12, 32);
+            makeAndModelLabel.Name = "makeAndModelLabel";
+            makeAndModelLabel.Size = new Size(96, 15);
+            makeAndModelLabel.TabIndex = 0;
+            makeAndModelLabel.Text = "&Make and Model";
+            // 
+            // makeAndModelList
+            // 
+            makeAndModelList.FormattingEnabled = true;
+            makeAndModelList.ItemHeight = 15;
+            makeAndModelList.Items.AddRange(new object[] { "Chevy Silverado", "Ford F-150", "Toyota Yaris", "Honda Element" });
+            makeAndModelList.Location = new Point(12, 50);
+            makeAndModelList.Name = "makeAndModelList";
+            makeAndModelList.Size = new Size(120, 94);
+            makeAndModelList.TabIndex = 1;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(makeAndModelList);
+            Controls.Add(makeAndModelLabel);
             Name = "MainForm";
             Text = "Assignment 2";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label makeAndModelLabel;
+        private ListBox makeAndModelList;
     }
 }
